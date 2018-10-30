199cc779c9ad149d380628f524ba019d8dcc8953
Darius Foo <darius.foo.tw@gmail.com>
2018-10-31 00:04:18 +0800

    Tests for unification

---
+++ src/test/kotlin/com/github/dariusf/rpqlol/UnifyTest.kt
@@ -0,0 +1,46 @@
+package com.github.dariusf.rpqlol;
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class UnifyTest {
+
+  @Test(expected = UnificationFailure::class)
+  fun primitiveFailure() {
+    unify(Env(), Num(1), Num(2))
+  }
+
+  @Test
+  fun primitiveSuccess() {
+    assertEquals(Env(), unify(Env(), Num(1), Num(1)))
+  }
+
+  @Test
+  fun variables() {
+    assertEquals(Env("x" to Num(1)), unify(Env(), Var("x"), Num(1)))
+  }
+
+  @Test(expected = UnificationFailure::class)
+  fun conflictingVariables() {
+    unify(Env(),
+        Functor("a", arrayListOf(Var("x"), Var("x"))),
+        Functor("a", arrayListOf(Num(1), Num(2))))
+  }
+
+  @Test
+  fun multipleVariables() {
+    assertEquals(Env("x" to Num(1), "y" to Num(1)),
+        unify(Env(),
+            Functor("a", arrayListOf(Var("x"), Var("y"))),
+            Functor("a", arrayListOf(Num(1), Var("x")))))
+  }
+
+  @Test
+  fun noResolveAll() {
+    assertEquals(Env("x" to Var("y"), "y" to Num(1)),
+        unify(Env(),
+            Functor("a", arrayListOf(Var("a"), Var("a"))),
+            Functor("a", arrayListOf(Var("b"), Num(1)))))
+  }
+}
+
